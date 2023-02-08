76832590f5979fdb22c6d9dc2c41941f3215062b
olgareuts <olga.re@mail.ru>
2023-02-08 10:31:53 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result;
+  let counter = 0;
+  while (counter !== bracketsConfig.length) {
+    bracketsConfig.forEach (el => {
+      brackets=el[0] + el[1];
+      let pos = str.indexOf(brackets);
+      if (pos !== -1) {
+        str = str.substring(0, pos) + str.substring(pos + 2, str.length);
+      } else {
+        counter++;  
+      }
+    })
+    if (counter === bracketsConfig.length) {
+      result = (str.length) ? 0 : 1;
+    } else {
+      counter = 0;
+    }
+  }
+  return result;
 }
+
+
